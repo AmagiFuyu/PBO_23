e6722d46d90ab1b722719febfc64b4f791805686
AmagiFuyu <114065239+AmagiFuyu@users.noreply.github.com>
2023-01-11 10:13:04 +0700

    Script Java

---
+++ HelloWorld.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Benny Alaster
+ */
+public class HelloWorld {
+    public static void main(string[] args){
+        System.out.println("Hello World");
+        System.out.println("Welcome");
+    }
+
+    private static class string {
+
+        public string() {
+        }
+    }
+}
